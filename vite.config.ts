e33fc4951da30ab60b0e2257f6995c023d1ae214
Faran <faran2093@gmail.com>
2026-01-02 15:54:55 -0600

    Add configs for github page hosting

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/zelij-labs-showcase/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
